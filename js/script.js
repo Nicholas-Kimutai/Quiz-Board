e38276e23f333fd627281c6faef4432715a4ab4b
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-02-06 03:10:56 +0300

    chore: Declared functions in the scripting file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function totalScore(){
+  var questionOne=document.getElementsByName("question1");
+  var questionTwo=document.getElementsByName("question2");
+  var questionThree=document.getElementsByName("question3");
+  var questionFour=document.getElementsByClassName("question4");
+  var questionFive=document.getElementsByClassName("question5");
+  var score=0;
+}
